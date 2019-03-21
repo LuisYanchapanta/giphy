79f80077cf7c828794a481029b5bd8d751148575
LuisYanchapanta <yanchapanta@aclanyc.com>
2019-03-21 22:26:00 +0000

    made gif work

--- js/script.js
+++ js/script.js
@@ -4,7 +4,19 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+   var searchInput = $("input").val();
+    console.log(searchInput);
+    var request_url = "https://api.giphy.com/v1/gifs/search?q= " + searchInput + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    
+    $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response) {
+           var pic_url = response.data[0].images.original.url;
+           $("#my_image").attr("src", pic_url);
+         //  $('body').append('<img src=' + pic_url + '>');
+      },
+    });
   
   
 });
