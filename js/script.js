b6c34e12cd8e2cb9d4f50fa5cb6da280bb031335
LuisYanchapanta <yanchapanta@aclanyc.com>
2019-03-28 22:29:19 +0000

    added new gifs

--- js/script.js
+++ js/script.js
@@ -7,17 +7,20 @@ $("#search-button").click(function(){
    var searchInput = $("input").val();
     console.log(searchInput);
     var request_url = "https://api.giphy.com/v1/gifs/search?q= " + searchInput + "&rating=pg&api_key=dc6zaTOxFJmzC";
-    
+
     $.ajax({
       url: request_url,
       method: "GET",
       success: function(response) {
-           var pic_url = response.data[0].images.original.url;
+          var extend = (response.data.length - 1) * Math.random();
+           extend = Math.floor(extend);
+           console.log(extend);
+           var pic_url = response.data[extend].images.original.url;
            $("#my_image").attr("src", pic_url);
          //  $('body').append('<img src=' + pic_url + '>');
       },
     });
-  
-  
+
+
 });
 
